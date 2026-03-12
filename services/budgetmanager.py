bf26949da701d03f596f626356d25e7931d9052d
myahye604 <moman605@gmail.com>
2026-03-12 02:00:46 -0700

    Complete StorageHandler implementation
    - Add save/load/serialize/deserialize logic
    
    - Update BudgetManager to use StorageHandler for persistence
    
    - Verify transactions persist correctly between runs

--- services/budgetmanager.py
+++ services/budgetmanager.py
@@ -3,14 +3,17 @@ from services.storagehandler import storageHandler
 
 class BudgetManager:
 
+    
+
     def __init__(self, storage):
+        self._storage = storage
         self._transactions = storage.load() #loads current list of transactions, if nothing method returns empty list
 
     def add_transaction(self, _sTransaction):
         self._transactions.append(_sTransaction) # type: ignore
+        self._storage.save(self._transactions)
 
     def get_total_income(self):
-
         #for every transaction in our list, if it is of type income, we add the amounts
         total_inc = 0
         for t in self._transactions:
